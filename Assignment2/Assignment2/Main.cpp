982fa5582074a59fafd6d7f24d0a53b9097096e6
Wizner <wizner8398@fvtc.edu>
2013-09-03 18:08:01 -0500

    initial checkin or assignment2

---
+++ Assignment2/Assignment2/Main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <conio.h>
+#include <iomanip>
+#include <Windows.h>
+
+using namespace std;
+
+
+int main()
+{
+	//write first line of output and then end the line
+	cout << "Computers, computers everywhere" << endl;
+
+	//write second line of output and then end the line
+	cout << "  as far as I can C." << endl;
+
+	//write third line of output and then end the line
+	cout << "I really, really like these things." << endl;
+
+	//write fourth line of output and then end the line
+	cout << "  Oh joy.  Oh joy for me!";
+
+	_getch();
+	return 0;
+}
