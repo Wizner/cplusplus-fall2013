20dc43619b5a1ca869bf1574ec4e0ea7667d9a01
Wizner <wizner8398@fvtc.edu>
2013-09-10 09:37:39 -0500

    added new example

--- Assignment3/Assignment3/Main.cpp
+++ Assignment3/Assignment3/Main.cpp
@@ -2,11 +2,13 @@
 #include <conio.h>
 #include <iomanip>
 #include <Windows.h>
-
+#include <string>
 using namespace std;
 
-void printOutput(string, float)
+void printOutput(string, float);
 string formatMoney(float);
+int COLUMN_ONE_WIDTH = 10;
+int COLUMN_TWO_WIDTH = 10;
 
 int main()
 {
@@ -15,22 +17,11 @@ int main()
 	float price3 = 65.40f;
 	float price4 = 103.45f;
 	char temp_price[16];
-	
-	int column_one_width = 10;
-	int column_two_width = 10;
-		
+			
 	cout << fixed << setprecision(2);
 	
-	cout << left << setw(column_one_width) << "PART NO." << right << setw(column_two_width) << "PRICE" << endl;
-			
-	/*cout << left << setw(column_one_width) << "T1267" << right << setw(column_two_width) << temp_price << endl;
+	cout << left << setw(COLUMN_ONE_WIDTH) << "PART NO." << right << setw(COLUMN_TWO_WIDTH) << "PRICE" << endl;
 		
-	cout << left << setw(column_one_width) << "T1300" << right << setw(column_two_width) << temp_price << endl;
-		
-	cout << left << setw(column_one_width) << "T2401" << right << setw(column_two_width) << temp_price << endl;
-		
-	cout << left << setw(column_one_width) << "T4482" << right << setw(column_two_width) << temp_price << endl;
-	*/
 	printOutput("T1267", price1);
 	printOutput("T1300", price2);
 	printOutput("T2401", price3);
@@ -42,8 +33,7 @@ int main()
 
 void printOutput(string partNum, float price)
 {	
-	cout << left << setw(COLUMN_ONE_WIDTH) << partNum 
-		<< right << setw(COLUMN_THREE_WIDTH) << formatMoney(price) << endl;
+	cout << left << setw(COLUMN_ONE_WIDTH) << partNum << right << setw(COLUMN_TWO_WIDTH) << formatMoney(price) << endl;
 }
 
 string formatMoney(float price)
@@ -51,6 +41,6 @@ string formatMoney(float price)
 	//create a temporary character array
 	char temp_price[16];
 	//use sprintf to format the string two decimal places
-	sprintf_s(temp_price, "$%.2f", price * 100);	
+	sprintf_s(temp_price, "$%.2f", price);	
 	return temp_price;
 }
