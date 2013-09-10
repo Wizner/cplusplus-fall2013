c10e7f97f077c3dbfb25fa3169819d16098ffcd0
Wizner <wizner8398@fvtc.edu>
2013-09-10 10:15:22 -0500

    initial commit for assignment 4

---
+++ Assignment4/Assignment4/Main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <conio.h>
+#include <iomanip>
+#include <Windows.h>
+#include <string>
+using namespace std;
+
+void printOutput(string, float);
+
+const int COLUMN_ONE_WIDTH = 20;
+const int COLUMN_TWO_WIDTH = 10;
+
+
+int main()
+{
+	float first_num = 106.54;
+	float second_num = 89.532;
+	float third_num = 98.76;
+	float total = first_num + second_num + third_num;
+	float average = total / 3;	
+		
+	cout << left << setw(COLUMN_ONE_WIDTH) << "First number is: " << right << setw(COLUMN_TWO_WIDTH) << first_num << endl;
+	cout << left << setw(COLUMN_ONE_WIDTH) << "Second number is: " << right << setw(COLUMN_TWO_WIDTH) << second_num << endl;
+	cout << left << setw(COLUMN_ONE_WIDTH) << "Third number is: " << right << setw(COLUMN_TWO_WIDTH) << third_num << endl;
+	cout << left << setw(COLUMN_ONE_WIDTH) << "Average is: " << right << setw(COLUMN_TWO_WIDTH) << printf("%.1f", average) << endl;
+	cout << left << setw(COLUMN_ONE_WIDTH) << "Total is: " << right << setw(COLUMN_TWO_WIDTH) << printf("%.4f", total) << endl;
+	
+	_getch();
+	return 0;
+}
+
+
